f439467ba75fac2254a132eee779d6a2d1bab063
Bui Luong Bich Hanh <buihanh07102001@gmail.com>
2023-12-13 20:48:33 +0700

    connect api and get image  profile

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import './sidebar.css'
-import SidebarButton from './sidebarButton'
-import { MdFavorite } from 'react-icons/md'
-import { FaGripfire, FaPlay } from 'react-icons/fa'
-import { FaSignOutAlt } from 'react-icons/fa'
-import { IoLibrary } from 'react-icons/io5'
-import { MdSpaceDashboard } from 'react-icons/md'
+import React, { useState, useEffect } from "react";
+import "./sidebar.css";
+import SidebarButton from "./sidebarButton";
+import { MdFavorite } from "react-icons/md";
+import { FaGripfire, FaPlay } from "react-icons/fa";
+import { FaSignOutAlt } from "react-icons/fa";
+import { IoLibrary } from "react-icons/io5";
+import { MdSpaceDashboard } from "react-icons/md";
+import apiClient from "../../spotify";
 
 export default function Sidebar() {
+  const [image, setImage] = useState(
+    "https://kenh14cdn.com/203336854389633024/2022/4/14/photo-1-1649929053290514407805.png"
+  );
+  useEffect(() => {
+    apiClient.get("me").then((response) => {
+      setImage(response.data.images[0].url);
+    });
+  }, []);
   return (
     <div className='sidebar-container'>
       <img
-        src='https://kenh14cdn.com/203336854389633024/2022/4/14/photo-1-1649929053290514407805.png'
+        src={image}
         className='profile-img'
         alt='rosé' />
       <div>
